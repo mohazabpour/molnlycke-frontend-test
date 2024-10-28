311e04764e7059f8bdfc8b016a0a2384eb839d89
Mohazabpour <ali.m@unifiedcomputing.com.au>
2024-10-28 23:42:42 +0100

    creates topbar component

---
+++ src/app/layout/topbar/top-bar.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, ElementRef, ViewChild} from '@angular/core';
+import {LayoutService} from '@app/layout/services/layout.service';
+import {Order} from '@app/api/order';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TopBarComponent {
+  items!: MenuItem[];
+
+  orders: Order[] = [];
+
+  @ViewChild('menubutton') menuButton!: ElementRef;
+
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+
+  @ViewChild('topbarmenu') menu!: ElementRef;
+
+  constructor(public layoutService: LayoutService) {}
+
+}
