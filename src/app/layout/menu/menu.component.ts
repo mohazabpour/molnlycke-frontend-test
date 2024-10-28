e7cae29b785391dc7e15b1870619ab79ccc3c3ec
Mohazabpour <ali.m@unifiedcomputing.com.au>
2024-10-28 23:43:10 +0100

    creates menu component

---
+++ src/app/layout/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MenuComponent {
+
+}
