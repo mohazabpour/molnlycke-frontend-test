bc8fa8aa6a551f7c9d2363c2f9ddc73ca4f8c02e
Mohazabpour <ali.m@unifiedcomputing.com.au>
2024-10-30 00:59:16 +0100

    update dashboard component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {NewsCardService} from '@components/news-card/news-card.service';
+import {ProductsService} from '@components/product-card/products.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
+  private newsCardService = inject(NewsCardService);
+  private productsService = inject(ProductsService);
 
+  //NEWS
+  newsCards = this.newsCardService.news;
+
+  //PRODUCTS
+  products = this.productsService.products;
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,12 +2,31 @@ import {NgModule} from '@angular/core';
 import {DashboardComponent} from '@app/pages/dashboard/dashboard.component';
 import {CommonModule} from '@angular/common';
 import {DashboardsRoutingModule} from '@app/pages/dashboard/dashboard-routing.module';
+import {CarouselModule} from 'primeng/carousel';
+import {NewsCardComponent} from '@components/news-card/news-card.component';
+import {SplitterModule} from 'primeng/splitter';
+import {NewsCardService} from '@components/news-card/news-card.service';
+import {TabViewModule} from 'primeng/tabview';
+import {ProductsService} from '@components/product-card/products.service';
+import {provideHttpClient} from '@angular/common/http';
+import {ProductCardComponent} from '@components/product-card/product-card.component';
+import {CalculatorComponent} from '@components/calculator-widget/calculator.component';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
-    imports: [
-      CommonModule,
-      DashboardsRoutingModule
-    ],
-    declarations: [DashboardComponent]
+  imports: [
+    CommonModule,
+    DashboardsRoutingModule,
+    CarouselModule,
+    NewsCardComponent,
+    SplitterModule,
+    TabViewModule,
+    ProductCardComponent,
+    CalculatorComponent,
+    CardModule,
+  ],
+  declarations: [DashboardComponent],
+  providers: [provideHttpClient(), NewsCardService, ProductsService],
+
 })
 export class DashboardModule {}
